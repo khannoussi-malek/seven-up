cbaa947c9766246171010c7bc781847cda99567f
khannoussi-malek <khannoussimalek@gmail.com>
2023-02-19 00:46:04 +0100

    add input filed

---
+++ seven-up/src/components/FiledText/index.js
@@ -0,0 +1,48 @@
+import { useField } from "@formiz/core";
+import { useState } from 'react';
+
+const FieldText = (props) => {
+    const {
+      errorMessage,
+      id,
+      isValid,
+      isPristine,
+      isSubmitted,
+      resetKey,
+      setValue,
+      value,
+    } = useField(props)
+    const { label, type, required } = props
+    const [isFocused, setIsFocused] = useState(false);
+    const showError = !isValid && !isFocused && (!isPristine || isSubmitted)
+  
+    return (
+      <div className={`demo-form-group ${showError ? 'is-error' : ''}`}>
+        <label
+          className="demo-label"
+          htmlFor={id}
+        >
+          { label }
+          {required && ' *'}
+        </label>
+        <input
+          key={resetKey}
+          id={id}
+          type={type || 'text'}
+          value={value || ''}
+          onChange={e => setValue(e.target.value)}
+          onFocus={() => setIsFocused(true)}
+          onBlur={() => setIsFocused(false)}
+          aria-invalid={!isValid}
+          aria-describedby={!isValid ? `error-${id}` : null}
+        />
+        {showError && (
+          <div id={`${id}`}>
+            { errorMessage }
+          </div>
+        )}
+      </div>
+    )
+  }
+
+  export default FieldText
