d51189ebfe1ed71ce092fd146e1786d4a71e4de1
Mamadobh <mohamedbenhafsia2001@gmail.com>
2023-02-19 07:46:35 +0000

    update singup

--- seven-up/src/components/FiledText/index.js
+++ seven-up/src/components/FiledText/index.js
@@ -1,48 +1,42 @@
 import { useField } from "@formiz/core";
-import { useState } from 'react';
+import { useState } from "react";
 
 const FieldText = (props) => {
-    const {
-      errorMessage,
-      id,
-      isValid,
-      isPristine,
-      isSubmitted,
-      resetKey,
-      setValue,
-      value,
-    } = useField(props)
-    const { label, type, required } = props
-    const [isFocused, setIsFocused] = useState(false);
-    const showError = !isValid && !isFocused && (!isPristine || isSubmitted)
-  
-    return (
-      <div className={`demo-form-group ${showError ? 'is-error' : ''}`}>
-        <label
-          className="demo-label"
-          htmlFor={id}
-        >
-          { label }
-          {required && ' *'}
-        </label>
+  const {
+    errorMessage,
+    id,
+    isValid,
+    isPristine,
+    isSubmitted,
+    resetKey,
+    setValue,
+    value,
+  } = useField(props);
+  const { label, type, required } = props;
+  const [isFocused, setIsFocused] = useState(false);
+  const showError = !isValid && !isFocused && (!isPristine || isSubmitted);
+
+  return (
+    <div className={`demo-form-group ${showError ? "is-error" : ""}`}>
+      <label className="demo-label" htmlFor={id}>
+        {label}
+      </label>
+      <div style={{ diplay: "flex" ,fontSize:"15px"}}>
         <input
           key={resetKey}
           id={id}
-          type={type || 'text'}
-          value={value || ''}
-          onChange={e => setValue(e.target.value)}
+          type={type || "text"}
+          value={value || ""}
+          onChange={(e) => setValue(e.target.value)}
           onFocus={() => setIsFocused(true)}
           onBlur={() => setIsFocused(false)}
           aria-invalid={!isValid}
           aria-describedby={!isValid ? `error-${id}` : null}
         />
-        {showError && (
-          <div id={`${id}`}>
-            { errorMessage }
-          </div>
-        )}
+        {showError && <div id={`${id}`}>{errorMessage}</div>}
       </div>
-    )
-  }
+    </div>
+  );
+};
 
-  export default FieldText
+export default FieldText;
