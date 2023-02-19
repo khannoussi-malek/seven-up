7b4a54dacb857c85ba3f6033dcd5f669deff5bcc
Hadil Guerfi <hdlguerfi@gmail.com>
2023-02-19 06:31:53 +0100

    add side bar

--- seven-up/src/component/SideBar/SideBar.js
+++ seven-up/src/component/SideBar/SideBar.js
@@ -1,27 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./SideBar.css"
 import { Link } from 'react-router-dom'
-import { AiOutlineLogin } from "react-icons/ai"
+import { AiOutlineLogin ,AiFillLeftCircle,AiFillRightCircle } from "react-icons/ai"
 import { BsFillPersonFill } from "react-icons/bs"
 import { GoSignIn } from "react-icons/go"
-import logo from "../../Assets/logo.jpg"
 
 function SideBar() {
 
+    const [classDisp,setClassDisp]=useState(false)
+    const[classOpen,setClassOpen]=useState("");
+
+
+    const handleArrows=()=>{
+        setClassOpen(!classDisp?"open":"");
+        setClassDisp(!classDisp);
+        if(classDisp===false){
+
+        }
+
+    }
+    console.log({classOpen})
+
+
     return (
-        <div className='sideBar'>
-            <ul className='links'>
-                  <li>
-                    <div className='link'>
+        <div   className={`${classOpen} sideBar`}>
+            <ul  className={` links`}>
+
+            <li  className={`${classDisp?"disp":"hidden"} leftArr`} onClick={handleArrows}><AiFillLeftCircle/></li>
+           
+            <li className={`${!classDisp?"disp":"hidden"} rightArr`} onClick={handleArrows}><AiFillRightCircle/></li>
+                  <li >
+                    <div className={` link`}>
                         <AiOutlineLogin className='icons' />
-                        <Link to="login">Log In</Link>
+                        <Link  to="login" className={`${classDisp===true && "disp"} rightArr`}>Log In</Link>
                     </div>
                     </li>
 
                     <li>
                         <div className='link'>
                             <GoSignIn className='icons' />
-                            <Link to="signin">Sign In</Link>
+                            <Link to="signin" className={`${classDisp===true && "disp"}`}>Sign In</Link>
                         </div>
                     </li>
 
@@ -30,7 +48,7 @@ function SideBar() {
                 <li>
                     <div className='link'>
                         <BsFillPersonFill className='icons' />
-                        <Link to="Q&A">Q&A</Link>
+                        <Link to="Q&A" className={`${classDisp===true && "disp"}`}>Q&A</Link>
                     </div>
                 </li>
 
